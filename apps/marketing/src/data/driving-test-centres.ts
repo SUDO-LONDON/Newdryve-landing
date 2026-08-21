b643303da89d0075f620d35757fc8585cd28f5ab
sinan <160494951+sguckiran@users.noreply.github.com>
2026-08-21 18:02:32 +0100

    Switch instructor areas to test centres
    Replaces the apply form’s single “Primary city” select with checkbox pills for primary practical test centres, adds step validation requiring at least one selection, and submits selected centres in the payload. To preserve current backend behavior, `service_areas` is now sent as `['Norwich', ...selectedCentres]` so the legacy city anchor still works while capturing more useful centre-level coverage.
    
    Also adds a new `driving-test-centres.ts` dataset containing active DVSA practical test centres grouped by city/locality (DRT122F, July 2026) for use in centre-based flows.

---
+++ apps/marketing/src/data/driving-test-centres.ts
@@ -0,0 +1,921 @@
+/**
+ * Active DVSA car practical driving test centres, grouped by the locality in
+ * the official centre name. Source: DRT122F, July 2026.
+ *
+ * https://www.gov.uk/government/statistical-data-sets/driving-test-and-theory-test-data-cars
+ */
+export const testCentresByCity = {
+  "Aberdeen North": [
+    "Aberdeen North"
+  ],
+  "Aberdeen South": [
+    "Aberdeen South (Cove)"
+  ],
+  "Aberfeldy": [
+    "Aberfeldy"
+  ],
+  "Abergavenny": [
+    "Abergavenny"
+  ],
+  "Aberystwyth": [
+    "Aberystwyth (Park Avenue)"
+  ],
+  "Airdrie": [
+    "Airdrie"
+  ],
+  "Alness": [
+    "Alness"
+  ],
+  "Alnwick": [
+    "Alnwick"
+  ],
+  "Arbroath": [
+    "Arbroath"
+  ],
+  "Ashfield": [
+    "Ashfield"
+  ],
+  "Ashford": [
+    "Ashford (Kent)"
+  ],
+  "Atherton": [
+    "Atherton (Manchester)"
+  ],
+  "Aylesbury": [
+    "Aylesbury"
+  ],
+  "Ayr": [
+    "Ayr"
+  ],
+  "Bala": [
+    "Bala"
+  ],
+  "Ballater": [
+    "Ballater"
+  ],
+  "Banbury": [
+    "Banbury"
+  ],
+  "Banff": [
+    "Banff"
+  ],
+  "Bangor": [
+    "Bangor"
+  ],
+  "Barnet": [
+    "Barnet (London)"
+  ],
+  "Barnsley": [
+    "Barnsley"
+  ],
+  "Barnstaple": [
+    "Barnstaple"
+  ],
+  "Barra": [
+    "Barra"
+  ],
+  "Barrow In Furness": [
+    "Barrow In Furness"
+  ],
+  "Barry": [
+    "Barry"
+  ],
+  "Basildon": [
+    "Basildon"
+  ],
+  "Basingstoke": [
+    "Basingstoke"
+  ],
+  "Bedford": [
+    "Bedford"
+  ],
+  "Belvedere": [
+    "Belvedere (London)"
+  ],
+  "Benbecula Island": [
+    "Benbecula Island"
+  ],
+  "Berwick-On-Tweed": [
+    "Berwick-On-Tweed"
+  ],
+  "Birmingham": [
+    "Birmingham (Cocks Moors)",
+    "Birmingham (Garretts Green)",
+    "Birmingham (Kings Heath)",
+    "Birmingham (Kingstanding)",
+    "Birmingham (Shirley)",
+    "Birmingham (South Yardley)"
+  ],
+  "Bishopbriggs": [
+    "Bishopbriggs"
+  ],
+  "Bishops Stortford": [
+    "Bishops Stortford"
+  ],
+  "Blackburn with Darwen": [
+    "Blackburn with Darwen"
+  ],
+  "Blackpool": [
+    "Blackpool"
+  ],
+  "Bletchley": [
+    "Bletchley"
+  ],
+  "Blyth": [
+    "Blyth"
+  ],
+  "Bodmin": [
+    "Bodmin"
+  ],
+  "Bolton": [
+    "Bolton (Manchester)"
+  ],
+  "Borehamwood": [
+    "Borehamwood (London)"
+  ],
+  "Boston": [
+    "Boston"
+  ],
+  "Bradford": [
+    "Bradford (Heaton)",
+    "Bradford (Thornbury)"
+  ],
+  "Brecon": [
+    "Brecon"
+  ],
+  "Bredbury": [
+    "Bredbury (Manchester)"
+  ],
+  "Brentwood": [
+    "Brentwood (London)"
+  ],
+  "Bridgend": [
+    "Bridgend"
+  ],
+  "Bridlington": [
+    "Bridlington"
+  ],
+  "Bristol": [
+    "Bristol (Avonmouth)",
+    "Bristol (Kingswood)"
+  ],
+  "Bromley": [
+    "Bromley (London)"
+  ],
+  "Buckie": [
+    "Buckie"
+  ],
+  "Burgess Hill": [
+    "Burgess Hill"
+  ],
+  "Burton on Trent": [
+    "Burton on Trent"
+  ],
+  "Bury": [
+    "Bury (Manchester)"
+  ],
+  "Bury St Edmunds": [
+    "Bury St Edmunds"
+  ],
+  "Buxton": [
+    "Buxton"
+  ],
+  "Callander": [
+    "Callander"
+  ],
+  "Camborne": [
+    "Camborne"
+  ],
+  "Cambridge": [
+    "Cambridge (Brookmount Court)"
+  ],
+  "Campbeltown": [
+    "Campbeltown"
+  ],
+  "Canterbury": [
+    "Canterbury"
+  ],
+  "Cardiff": [
+    "Cardiff (Llanishen)"
+  ],
+  "Cardigan": [
+    "Cardigan"
+  ],
+  "Carlisle": [
+    "Carlisle"
+  ],
+  "Carlisle LGV": [
+    "Carlisle LGV (Cars)"
+  ],
+  "Carmarthen": [
+    "Carmarthen"
+  ],
+  "Castle Douglas": [
+    "Castle Douglas"
+  ],
+  "Chadderton": [
+    "Chadderton"
+  ],
+  "Cheetham Hill": [
+    "Cheetham Hill (Manchester)"
+  ],
+  "Chelmsford": [
+    "Chelmsford"
+  ],
+  "Cheltenham": [
+    "Cheltenham"
+  ],
+  "Chertsey": [
+    "Chertsey (London)"
+  ],
+  "Chester": [
+    "Chester"
+  ],
+  "Chesterfield": [
+    "Chesterfield"
+  ],
+  "Chichester": [
+    "Chichester"
+  ],
+  "Chingford": [
+    "Chingford (London)"
+  ],
+  "Chippenham": [
+    "Chippenham"
+  ],
+  "Chorley": [
+    "Chorley"
+  ],
+  "Clacton-on-Sea": [
+    "Clacton-on-Sea"
+  ],
+  "Colchester": [
+    "Colchester"
+  ],
+  "Coventry": [
+    "Coventry"
+  ],
+  "Crawley": [
+    "Crawley"
+  ],
+  "Crewe": [
+    "Crewe"
+  ],
+  "Crieff": [
+    "Crieff"
+  ],
+  "Cumnock": [
+    "Cumnock"
+  ],
+  "Darlington": [
+    "Darlington"
+  ],
+  "Derby": [
+    "Derby (Alvaston)"
+  ],
+  "Doncaster": [
+    "Doncaster"
+  ],
+  "Dorchester": [
+    "Dorchester"
+  ],
+  "Dudley": [
+    "Dudley"
+  ],
+  "Dumbarton": [
+    "Dumbarton"
+  ],
+  "Dumfries": [
+    "Dumfries"
+  ],
+  "Dundee": [
+    "Dundee"
+  ],
+  "Dunfermline": [
+    "Dunfermline (Vine)"
+  ],
+  "Dunoon": [
+    "Dunoon"
+  ],
+  "Duns": [
+    "Duns"
+  ],
+  "Durham": [
+    "Durham"
+  ],
+  "East Kilbride": [
+    "East Kilbride"
+  ],
+  "Eastbourne": [
+    "Eastbourne"
+  ],
+  "Edinburgh": [
+    "Edinburgh (Currie)",
+    "Edinburgh (Musselburgh)"
+  ],
+  "Elgin": [
+    "Elgin"
+  ],
+  "Enfield": [
+    "Enfield (Brancroft Way)",
+    "Enfield (Innova Business Park)"
+  ],
+  "Erith": [
+    "Erith (London)"
+  ],
+  "Exeter": [
+    "Exeter"
+  ],
+  "Farnborough": [
+    "Farnborough"
+  ],
+  "Featherstone": [
+    "Featherstone"
+  ],
+  "Folkestone": [
+    "Folkestone"
+  ],
+  "Forfar": [
+    "Forfar"
+  ],
+  "Fort William": [
+    "Fort William"
+  ],
+  "Fraserburgh": [
+    "Fraserburgh"
+  ],
+  "Gairloch": [
+    "Gairloch"
+  ],
+  "Galashiels": [
+    "Galashiels"
+  ],
+  "Gateshead": [
+    "Gateshead"
+  ],
+  "Gillingham": [
+    "Gillingham"
+  ],
+  "Girvan": [
+    "Girvan"
+  ],
+  "Glasgow": [
+    "Glasgow (Anniesland)",
+    "Glasgow (Baillieston)",
+    "Glasgow (Shieldhall)"
+  ],
+  "Gloucester": [
+    "Gloucester"
+  ],
+  "Golspie": [
+    "Golspie"
+  ],
+  "Goodmayes": [
+    "Goodmayes (London)"
+  ],
+  "Gosforth": [
+    "Gosforth"
+  ],
+  "Grangemouth": [
+    "Grangemouth"
+  ],
+  "Grantham": [
+    "Grantham (Somerby)"
+  ],
+  "Grantown-On-Spey": [
+    "Grantown-On-Spey"
+  ],
+  "Greenford": [
+    "Greenford (Horsenden Lane)"
+  ],
+  "Greenham": [
+    "Greenham"
+  ],
+  "Greenock": [
+    "Greenock"
+  ],
+  "Grimsby Coldwater": [
+    "Grimsby Coldwater"
+  ],
+  "Guildford": [
+    "Guildford"
+  ],
+  "Haddington": [
+    "Haddington"
+  ],
+  "Halifax": [
+    "Halifax"
+  ],
+  "Hamilton": [
+    "Hamilton"
+  ],
+  "Hartlepool": [
+    "Hartlepool"
+  ],
+  "Hastings": [
+    "Hastings (Ore)"
+  ],
+  "Hawick": [
+    "Hawick"
+  ],
+  "Heckmondwike": [
+    "Heckmondwike"
+  ],
+  "Hendon": [
+    "Hendon (London)"
+  ],
+  "Hereford": [
+    "Hereford"
+  ],
+  "Herne Bay": [
+    "Herne Bay"
+  ],
+  "Hexham": [
+    "Hexham"
+  ],
+  "Heysham": [
+    "Heysham"
+  ],
+  "High Wycombe": [
+    "High Wycombe"
+  ],
+  "Hinckley": [
+    "Hinckley"
+  ],
+  "Hornchurch": [
+    "Hornchurch (London)"
+  ],
+  "Horsforth": [
+    "Horsforth"
+  ],
+  "Huddersfield": [
+    "Huddersfield"
+  ],
+  "Hull": [
+    "Hull"
+  ],
+  "Huntly": [
+    "Huntly"
+  ],
+  "Inveraray": [
+    "Inveraray"
+  ],
+  "Inverness": [
+    "Inverness (Longman Drive)"
+  ],
+  "Inverurie": [
+    "Inverurie"
+  ],
+  "Ipswich": [
+    "Ipswich"
+  ],
+  "Irvine": [
+    "Irvine"
+  ],
+  "Isle of Mull": [
+    "Isle of Mull"
+  ],
+  "Isle of Skye": [
+    "Isle of Skye (Portree)"
+  ],
+  "Isleworth": [
+    "Isleworth (Fleming Way)"
+  ],
+  "Kelso": [
+    "Kelso"
+  ],
+  "Kendal": [
+    "Kendal (Oxenholme Road)"
+  ],
+  "Kettering": [
+    "Kettering"
+  ],
+  "Kings Lynn": [
+    "Kings Lynn"
+  ],
+  "Kingussie": [
+    "Kingussie"
+  ],
+  "Kirkcaldy": [
+    "Kirkcaldy"
+  ],
+  "Knaresborough": [
+    "Knaresborough"
+  ],
+  "Kyle of Lochalsh": [
+    "Kyle of Lochalsh"
+  ],
+  "Lanark": [
+    "Lanark"
+  ],
+  "Launceston": [
+    "Launceston"
+  ],
+  "Lee On The Solent": [
+    "Lee On The Solent"
+  ],
+  "Leeds": [
+    "Leeds (Colton Mill)",
+    "Leeds (Fearnville)"
+  ],
+  "Leicester": [
+    "Leicester (Cannock Street)",
+    "Leicester (Wigston)"
+  ],
+  "Leighton Buzzard": [
+    "Leighton Buzzard (Stanbridge Road)"
+  ],
+  "Lerwick": [
+    "Lerwick"
+  ],
+  "Letchworth": [
+    "Letchworth"
+  ],
+  "Lichfield": [
+    "Lichfield"
+  ],
+  "Lincoln": [
+    "Lincoln"
+  ],
+  "Livingston": [
+    "Livingston"
+  ],
+  "Llanelli": [
+    "Llanelli"
+  ],
+  "Llantrisant": [
+    "Llantrisant"
+  ],
+  "Lochgilphead": [
+    "Lochgilphead"
+  ],
+  "Loughborough": [
+    "Loughborough"
+  ],
+  "Loughton": [
+    "Loughton (London)"
+  ],
+  "Louth": [
+    "Louth"
+  ],
+  "Lowestoft": [
+    "Lowestoft(Mobbs Way)"
+  ],
+  "Ludlow": [
+    "Ludlow"
+  ],
+  "Luton": [
+    "Luton"
+  ],
+  "Macclesfield": [
+    "Macclesfield"
+  ],
+  "Maidstone": [
+    "Maidstone"
+  ],
+  "Mallaig": [
+    "Mallaig"
+  ],
+  "Malton": [
+    "Malton"
+  ],
+  "Medway Ambley Road": [
+    "Medway Ambley Road"
+  ],
+  "Melton Mowbray": [
+    "Melton Mowbray"
+  ],
+  "Merthyr Tydfil": [
+    "Merthyr Tydfil"
+  ],
+  "Middlesbrough": [
+    "Middlesbrough"
+  ],
+  "Mill Hill": [
+    "Mill Hill (London)"
+  ],
+  "Mitcham": [
+    "Mitcham (London)"
+  ],
+  "Monmouth": [
+    "Monmouth"
+  ],
+  "Montrose": [
+    "Montrose"
+  ],
+  "Morden": [
+    "Morden (London)"
+  ],
+  "Nelson": [
+    "Nelson"
+  ],
+  "Newport, Gwent": [
+    "Newport (Gwent)"
+  ],
+  "Newport, Isle of Wight": [
+    "Newport (Isle of Wight)"
+  ],
+  "Newton Abbot": [
+    "Newton Abbot"
+  ],
+  "Newton Stewart": [
+    "Newton Stewart"
+  ],
+  "Newtown": [
+    "Newtown"
+  ],
+  "Norris Green": [
+    "Norris Green (Liverpool)"
+  ],
+  "Northallerton": [
+    "Northallerton"
+  ],
+  "Northampton": [
+    "Northampton"
+  ],
+  "Northwich": [
+    "Northwich"
+  ],
+  "Norwich": [
+    "Norwich (Peachman Way)"
+  ],
+  "Nottingham": [
+    "Nottingham (Chilwell)",
+    "Nottingham (Colwick)"
+  ],
+  "Nuneaton": [
+    "Nuneaton"
+  ],
+  "Oban": [
+    "Oban"
+  ],
+  "Orkney": [
+    "Orkney"
+  ],
+  "Oswestry": [
+    "Oswestry"
+  ],
+  "Oxford": [
+    "Oxford (Cowley)"
+  ],
+  "Paisley": [
+    "Paisley"
+  ],
+  "Peebles": [
+    "Peebles"
+  ],
+  "Pembroke Dock": [
+    "Pembroke Dock"
+  ],
+  "Penzance": [
+    "Penzance"
+  ],
+  "Perth": [
+    "Perth (Arran Road)"
+  ],
+  "Peterborough": [
+    "Peterborough"
+  ],
+  "Peterhead": [
+    "Peterhead"
+  ],
+  "Pinner": [
+    "Pinner (London)"
+  ],
+  "Pitlochry": [
+    "Pitlochry"
+  ],
+  "Plymouth": [
+    "Plymouth"
+  ],
+  "Pontefract": [
+    "Pontefract"
+  ],
+  "Poole": [
+    "Poole"
+  ],
+  "Portsmouth": [
+    "Portsmouth"
+  ],
+  "Preston": [
+    "Preston"
+  ],
+  "Pwllheli": [
+    "Pwllheli"
+  ],
+  "Reading": [
+    "Reading"
+  ],
+  "Redditch": [
+    "Redditch"
+  ],
+  "Redhill Aerodrome": [
+    "Redhill Aerodrome"
+  ],
+  "Rhyl": [
+    "Rhyl"
+  ],
+  "Rochdale": [
+    "Rochdale (Manchester)"
+  ],
+  "Rotherham": [
+    "Rotherham"
+  ],
+  "Rothesay": [
+    "Rothesay"
+  ],
+  "Rugby": [
+    "Rugby"
+  ],
+  "Sale": [
+    "Sale (Manchester)"
+  ],
+  "Salisbury": [
+    "Salisbury"
+  ],
+  "Scarborough": [
+    "Scarborough"
+  ],
+  "Scunthorpe": [
+    "Scunthorpe"
+  ],
+  "Sevenoaks": [
+    "Sevenoaks"
+  ],
+  "Sheffield": [
+    "Sheffield (Handsworth)",
+    "Sheffield (Middlewood Road)"
+  ],
+  "Shrewsbury": [
+    "Shrewsbury"
+  ],
+  "Sidcup": [
+    "Sidcup (London)"
+  ],
+  "Skegness": [
+    "Skegness"
+  ],
+  "Skipton": [
+    "Skipton"
+  ],
+  "Slough": [
+    "Slough (London)"
+  ],
+  "Southall": [
+    "Southall (London)"
+  ],
+  "Southampton": [
+    "Southampton (Maybush)"
+  ],
+  "Southend-on-Sea": [
+    "Southend-on-Sea"
+  ],
+  "Southport": [
+    "Southport (Liverpool)"
+  ],
+  "Speke": [
+    "Speke (Liverpool)"
+  ],
+  "St Albans": [
+    "St Albans"
+  ],
+  "St Helens": [
+    "St Helens (Liverpool)"
+  ],
+  "Stafford": [
+    "Stafford"
+  ],
+  "Steeton": [
+    "Steeton"
+  ],
+  "Stevenage": [
+    "Stevenage"
+  ],
+  "Stirling": [
+    "Stirling"
+  ],
+  "Stoke-On-Trent": [
+    "Stoke-On-Trent (Cobridge)"
+  ],
+  "Stoke-on-Trent": [
+    "Stoke-on-Trent (Newcastle-Under-Lyme)"
+  ],
+  "Stornoway": [
+    "Stornoway"
+  ],
+  "Stranraer": [
+    "Stranraer"
+  ],
+  "Sunderland": [
+    "Sunderland"
+  ],
+  "Swansea": [
+    "Swansea"
+  ],
+  "Swindon": [
+    "Swindon"
+  ],
+  "Taunton": [
+    "Taunton"
+  ],
+  "Telford": [
+    "Telford"
+  ],
+  "Tilbury": [
+    "Tilbury"
+  ],
+  "Tolworth": [
+    "Tolworth (London)"
+  ],
+  "Tottenham": [
+    "Tottenham"
+  ],
+  "Trowbridge": [
+    "Trowbridge"
+  ],
+  "Tunbridge Wells": [
+    "Tunbridge Wells"
+  ],
+  "Upton": [
+    "Upton"
+  ],
+  "Uxbridge": [
+    "Uxbridge (London)"
+  ],
+  "Wakefield": [
+    "Wakefield"
+  ],
+  "Wallasey": [
+    "Wallasey"
+  ],
+  "Walton LGV": [
+    "Walton LGV"
+  ],
+  "Wanstead": [
+    "Wanstead (London)"
+  ],
+  "Warrington": [
+    "Warrington"
+  ],
+  "Warwick": [
+    "Warwick (Wedgenock House)"
+  ],
+  "Watford": [
+    "Watford"
+  ],
+  "Wednesbury": [
+    "Wednesbury"
+  ],
+  "Wellingborough": [
+    "Wellingborough"
+  ],
+  "West Didsbury": [
+    "West Didsbury (Manchester)"
+  ],
+  "West Wickham": [
+    "West Wickham (London)"
+  ],
+  "Weston-super-Mare": [
+    "Weston-super-Mare"
+  ],
+  "Whitby": [
+    "Whitby"
+  ],
+  "Widnes": [
+    "Widnes"
+  ],
+  "Winchester": [
+    "Winchester"
+  ],
+  "Wolverhampton": [
+    "Wolverhampton"
+  ],
+  "Wood Green": [
+    "Wood Green (London)"
+  ],
+  "Worcester": [
+    "Worcester"
+  ],
+  "Workington": [
+    "Workington"
+  ],
+  "Worksop": [
+    "Worksop"
+  ],
+  "Worthing": [
+    "Worthing"
+  ],
+  "Wrexham": [
+    "Wrexham"
+  ],
+  "Yeading": [
+    "Yeading (London)"
+  ],
+  "Yeovil": [
+    "Yeovil"
+  ],
+  "York": [
+    "York"
+  ]
+} as const;
+
